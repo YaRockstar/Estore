0363851fc9421cfad3f2933c3b3d6b12d36ba4f0
ya_rockstar <yekalashnikov@gmail.com>
2023-05-24 18:47:46 +0300

    update: update import from render dir

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
-import { productService } from './services/productService.js';
 import { store } from './store/store.js';
+import { productService } from './services/productService.js';
 
-import { renderMainPage, renderMenu, renderCart } from './render/render.js';
+import * as Render from './render/render.js';
 
+const content = document.querySelector('.content');
 const logo = document.querySelector('.header__logo');
 const menuBtn = document.querySelector('.header__menu-btn');
-const content = document.querySelector('.content');
+const cartBtn = document.querySelector('.header__cart-btn');
 
 const start = async () => {};
 
 logo.addEventListener('click', () => {
-  renderMainPage(content);
+  Render.renderMainPage(content);
 });
 
 menuBtn.addEventListener('click', () => {
-  renderMenu(content, store.state.productList);
+  Render.renderMenu(content, store.state.productList);
+});
+
+cartBtn.addEventListener('click', event => {
+  const productId = event.target.dataset?.id;
+
+  if (productId) {
+  }
 });
 
 await start();
