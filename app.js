74fa42ece681b8075c5370dd3188b5640ffce8a4
ya_rockstar <yekalashnikov@gmail.com>
2023-05-24 23:31:15 +0300

    update: update some code

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ import { productService } from './services/productService.js';
 
 import { URL_PRODUCTS } from '../constants/constants.js';
 import * as HTTP from '../api/api.js';
-
 import * as Render from './render/render.js';
 
 const content = document.querySelector('.content');
@@ -15,8 +14,8 @@ const start = async () => {
   try {
     store.state.productList = await HTTP.getProducts(URL_PRODUCTS);
 
-    // const localStorageCart = JSON.parse(localStorage.getItem('cart'));
-    // store.state.cart = localStorageCart ? localStorageCart : store.state.cart;
+    const localStorageCart = JSON.parse(localStorage.getItem('cart'));
+    store.state.cart = localStorageCart ? localStorageCart : store.state.cart;
   } catch (e) {
     console.log(e.message);
     // productsContainer.innerHTML = `<p style="color: red">Error</p>`;
@@ -30,6 +29,27 @@ content.addEventListener('click', event => {
     const product = store.state.productList.find(p => p.id === productId);
     store.mutations.addToCart(product);
     localStorage.setItem('cart', JSON.stringify(store.state.cart));
+    return;
+  }
+
+  const categoryName = event.target.value;
+  const productListWrap = document.querySelector('.menu__product-list-wrap');
+
+  const seasonCategories = ['Летние', 'Осенние', 'Зимние', 'Весенние'];
+  const cookedCategories = ['Классические', 'Фирменные', 'Запеченные'];
+
+  if (seasonCategories.includes(categoryName)) {
+    const seasonList = store.state.productList.filter(
+      product => product.season_category_name === categoryName
+    );
+    Render.renderProducts(productListWrap, seasonList);
+  } else if (cookedCategories.includes(categoryName)) {
+    const cookedList = store.state.productList.filter(
+      product => product.cooked_category_name === categoryName
+    );
+    Render.renderProducts(productListWrap, cookedList);
+  } else {
+    Render.renderProducts(productListWrap, store.state.productList);
   }
 });
 
--- render/render.js
+++ render/render.js
@@ -19,10 +19,32 @@ export const renderMainPage = element => {
 export const renderMenu = (element, products) => {
   element.innerHTML = `
     <div class="content__menu">
-      <div></div>
-      ${htmlService.paintProducts(products)}
+      <div class="menu_categories">
+        Сезон:
+        <select name="season" class="menu_categories__season-select">
+          <option class="menu_categories__item" value="Выбрать все">Выбрать все</option>
+          <option class="menu_categories__item" value="Летние">Летние</option>
+          <option class="menu_categories__item" value="Осенние">Осенние</option>
+          <option class="menu_categories__item" value="Зимние">Зимние</option>
+          <option class="menu_categories__item" value="Весенние">Весенние</option>
+        </select>
+        Вид:
+        <select name="cooked" class="menu_categories__cooked-select">
+          <option class="menu_categories__item" value="Выбрать все">Выбрать все</option>
+          <option class="menu_categories__item" value="Классические">Классические</option>
+          <option class="menu_categories__item" value="Фирменные">Фирменные</option>
+          <option class="menu_categories__item" value="Запеченные">Запеченные</option>
+        </select>
+      </div>
+      <div class="menu__product-list-wrap">
+        ${htmlService.paintProducts(products)}
+      </div>
     </div>
   `;
+};
+
+export const renderProducts = (element, products) => {
+  element.innerHTML = htmlService.paintProducts(products);
 };
 
 export const renderCart = (element, cartItems) => {
