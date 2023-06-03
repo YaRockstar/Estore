aecc53a7269a5ee91d7fcd3c9e3af22749d300bb
ya_rockstar <yekalashnikov@gmail.com>
2023-06-03 23:22:49 +0300

    add: add gitignore file

--- constants/constants.js
+++
@@ -1,9 +0,0 @@
-export const URL = 'http://localhost:8888/Estore-server';
-
-export const URL_PRODUCTS = `${URL}/controllers/products.php`;
-
-export const URL_ORDER = `${URL}/controllers/order.php`;
-
-export const URL_AUTH = `${URL}/controllers/auth.php`;
-
-export const URL_REGISTER = `${URL}/controllers/register.php`;
